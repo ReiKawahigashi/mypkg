6ac1ee4461e2956823c2e00ff49f6922730dd066
rei <s24C1042LK@s.chibakoudai.jp>
2025-12-30 23:35:02 +0900

    Add a command

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -19,7 +19,11 @@ import pytest
 @pytest.mark.flake8
 @pytest.mark.linter
 def test_flake8():
-    rc, errors = main_with_errors(argv=[])
-    assert rc == 0, \
-        'Found %d code style errors / warnings:\n' % len(errors) + \
-        '\n'.join(errors)
+    rc, errors = flake8.main.cli.main(
+    argv=[
+        '.',
+        '--exclude=build,install,log'
+    ]
+)
+assert rc == 0
+
