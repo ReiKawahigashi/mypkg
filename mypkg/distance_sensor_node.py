b294ee8d1734ba0f305e6da96ee14e895f1c4f98
rei <s24C1042LK@s.chibakoudai.jp>
2025-12-30 22:59:54 +0900

    Fix flake8 W391

--- mypkg/distance_sensor_node.py
+++ mypkg/distance_sensor_node.py
@@ -1,20 +1,22 @@
-# distance_lid_light/distance_sensor_node.py
+import random
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
-import random
+
 
 class DistanceSensorNode(Node):
+
     def __init__(self):
         super().__init__('distance_sensor_node')
         self.publisher_ = self.create_publisher(Float32, 'distance', 10)
-        self.timer = self.create_timer(0.5, self.timer_callback)
+        self.timer = self.create_timer(1.0, self.timer_callback)
 
     def timer_callback(self):
         msg = Float32()
-        msg.data = random.uniform(0.0, 2.0)  # 距離[m]想定
+        msg.data = random.uniform(0.0, 100.0)
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Publish distance: {msg.data:.2f} m')
+
 
 def main():
     rclpy.init()
@@ -22,6 +24,7 @@ def main():
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
--- mypkg/lid_light_node.py
+++ mypkg/lid_light_node.py
@@ -1,29 +1,25 @@
-# distance_lid_light/lid_light_node.py
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
 
+
 class LidLightNode(Node):
+
     def __init__(self):
         super().__init__('lid_light_node')
         self.subscription = self.create_subscription(
             Float32,
             'distance',
-            self.callback,
-            10
+            self.listener_callback,
+            10,
         )
 
-    def callback(self, msg):
-        d = msg.data
-
-        if d < 0.5:
-            state = 'RED (Strong)'
-        elif d < 1.0:
-            state = 'YELLOW (Medium)'
+    def listener_callback(self, msg):
+        if msg.data < 30.0:
+            self.get_logger().info('LID LIGHT: ON')
         else:
-            state = 'GREEN (Weak)'
+            self.get_logger().info('LID LIGHT: OFF')
 
-        self.get_logger().info(f'Distance: {d:.2f} m → LED: {state}')
 
 def main():
     rclpy.init()
@@ -31,6 +27,7 @@ def main():
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
