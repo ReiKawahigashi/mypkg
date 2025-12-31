ddef68c07c60c31f40f62ef24a9fc7d130178de4
rei <s24C1042LK@s.chibakoudai.jp>
2025-12-31 17:31:18 +0900

    Fix flake8 and pep257 tests to limit directories

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -19,6 +19,7 @@ import pytest
 @pytest.mark.flake8
 @pytest.mark.linter
 def test_flake8():
+    # source code folders only
     rc, errors = main_with_errors(argv=[
         'mypkg',
         'launch',
