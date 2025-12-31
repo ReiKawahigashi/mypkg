4d390cf5130cdc16ab813f23b310222a8ea29c2c
rei <s24C1042LK@s.chibakoudai.jp>
2025-12-31 22:57:27 +0900

    Add a command

--- launch/random_to_alphabet.launch.py
+++ launch/random_to_alphabet.launch.py
@@ -5,12 +5,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='alphabet_pkg',
+            package='mypkg',
             executable='random_number_node',
             output='screen'
         ),
         Node(
-            package='alphabet_pkg',
+            package='mypkg',
             executable='alphabet_node',
             output='screen'
         ),
