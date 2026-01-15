d67a9c055d78140eebe782c0867b4c64f944d9fc
rei <s24C1042LK@s.chibakoudai.jp>
2026-01-15 15:18:52 +0900

    Add commit

--- test/test.bash
+++ test/test.bash
@@ -7,7 +7,8 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source $dir/.bashrc
+source /opt/ros/humble/setup.bash
+source install/setup.bash
 timeout 10 ros2 run mypkg double_calculator > /tmp/mypkg.log &
 
 sleep 2
