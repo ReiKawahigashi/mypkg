7e48011e668ef16caf76b745ebe8a2fa2858c849
rei <s24C1042LK@s.chibakoudai.jp>
2026-01-15 15:22:23 +0900

    Add commit

--- test/test.bash
+++ test/test.bash
@@ -9,11 +9,10 @@ cd $dir/ros2_ws
 colcon build
 source /opt/ros/humble/setup.bash
 source install/setup.bash
-timeout 10 ros2 run mypkg double_calculator > /tmp/mypkg.log &
+timeout 10 ros2 run mypkg double_calculator > /tmp/mypkg.log 2>&1 &
 
 sleep 2
 ros2 topic pub -1 /input_number std_msgs/msg/Int32 "{data: 10}"
 
 cat /tmp/mypkg.log |
 grep 'Published: 20'
-
